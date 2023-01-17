7cf52e4fbd59218d5ef925f047ae0e54bb2fffa9
Suhaib <suhainsaifan107@gmail.com>
2023-01-17 15:35:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import "./index"
 function App() {
+  const [data,setData] = useState([]);
+  const [toDo,setToDo] = useState("");
+  const [isEditable,setIsEditable] = useState(false);
+  const [editableItem,setEditableItem] = useState();
+
+
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault(); 
+    setData(prevData => [...prevData,{id:Math.random(),title:toDo}])
+  }
+
+  console.log("data",data);
+
+  const handleClick = ()=>{
+    data.map(i =>{
+      if (i.id === editableItem)
+        i.title = toDo;
+    })
+    setIsEditable(false);
+  }
+
+  const handleDelete =(id) =>{
+    const newData =data.filter(task =>{
+      return task.id !== id;
+    })
+    setData(newData);
+  }
+
+  const editClick = (id) =>{
+    setEditableItem(id)
+    setIsEditable(true)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='box-container'>
+        <h2 style={{textAlign:"center"}}>To Do</h2>
+        {isEditable?(
+          <div className='center'>
+          <input type="text" name="work"  onChange={(e) => setToDo(e.currentTarget.value)} />
+          <button type='button' onClick={handleClick}>edit</button>
+          </div>
+          ):(
+          <form onSubmit={handleSubmit} className='center'>
+            <input type="text" name="work"  onChange={(e) => setToDo(e.currentTarget.value)} />
+            <button type='submit' >Submit</button>   
+          </form>
+          )
+        }
+        {!!data.length && data.map(task=> (
+           <div className='task-container'>
+           <p >{task.title}</p>
+           <div>
+           <button onClick={() => handleDelete(task.id)}>Delete</button><button onClick={() => editClick(task.id)}>edit</button>
+           </div>
+            </div>
+        ))}
+      </div>
     </div>
   );
 }
